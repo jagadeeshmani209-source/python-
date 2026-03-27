8a6c3cbf3c66d730b128bb08c750851713ea34e1
jagadeeshmani209-source <jagadeeshmani209@gmail.com>
2026-03-27 15:01:50 +0530

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b:
+     if a>c:
+         print("A is larger")
+     else:
+         print("c is larger");
+else:
+    if b>c:
+         print("B is larger");
+    else:
+         print("C is larger");
---
+++ sum2.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: ")) 
+i = 1 
+total = 0
+
+while i <= num: 
+  total = total + i 
+  i += 1
+  
+print("Sum =", total)
